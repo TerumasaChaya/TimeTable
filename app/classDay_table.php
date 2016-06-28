5ecea503a6a0fdbd36880aae066eaeae695af713
田花聖太 <seita.1224.ds@outlook.com>
2016-06-28 14:57:39 +0900

    1日分の時間割表示機能追加中

--- app/classDay_table.php
+++ app/classDay_table.php
@@ -6,5 +6,21 @@ use Illuminate\Database\Eloquent\Model;
 
 class classDay_table extends Model
 {
-    //
+
+    protected $table = "classDay";
+
+    protected $fillabel = [
+        "id","class_Id","day","period","subject_Id"
+    ];
+    //hasMany設定
+    public function subject()
+    {
+        return $this->belongsTo('App\subject_table','id');
+
+    }
+    public function class_()
+    {
+        return $this->belongsTo('App\class_table');
+
+    }
 }
--- app/class_table.php
+++ app/class_table.php
@@ -6,5 +6,27 @@ use Illuminate\Database\Eloquent\Model;
 
 class class_table extends Model
 {
-    //
+
+    protected $table = "class";
+
+    protected $fillabel = [
+        "id","className","grade","dept","course","college","person"
+    ];
+//    public function classDay()
+//    {
+//        return $this->hasManyThrough('App\subject_table','App\classDay_table','class_Id');
+//
+//    }
+    //hasMany設定
+    public function classDay()
+    {
+        return $this->hasMany('App\classDay_table','class_Id');
+
+    }
+    //hasMany設定
+    public function users()
+    {
+        return $this->hasMany('App\users_table');
+
+    }
 }
