3b553fec6f6ea99c961cd5862566cfe042201e61
満若 峻弥 <2130007@maetel.ecc.ac.jp>
2016-07-19 15:34:08 +0900

    ビジュアル面での細かな微調整

--- resources/views/admin/register.blade.php
+++ resources/views/admin/register.blade.php
@@ -17,85 +17,6 @@
         </div>
         <hr />
 
-        <form action="{{ url('/admin/register') }}" class="form-horizontal" method="POST" role="form">
-            {{ csrf_field() }}
-            <div class="form-body pal">
-
-                <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
-                    <label for="name" class="col-md-4 control-label">名前</label>
-
-                    <div class="col-md-6">
-                        <div class="input-icon right">
-                            <i class="fa fa-user"></i>
-                            <input id="name" type="text" class="form-control" name="name" value="{{ old('name') }}">
-                        </div>
-
-                        @if ($errors->has('name'))
-                            <span class="help-block">
-                                        <strong>{{ $errors->first('name') }}</strong>
-                                    </span>
-                        @endif
-                    </div>
-                </div>
-
-                <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                    <label for="email" class="col-md-4 control-label">
-                        メールアドレス:</label>
-                    <div class="col-md-6">
-                        <input id="email" type="email" placeholder="" class="form-control" name="email" value="{{ old('email') }}"/>
-                        @if ($errors->has('email'))
-                            <span class="help-block">
-                                            <strong>{{ $errors->first('email') }}</strong>
-                                        </span>
-                        @endif
-                    </div>
-                </div>
-
-
-
-                <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                    <label for="password" class="col-md-4 control-label">パスワード：</label>
-
-                    <div class="col-md-6">
-                        <div class="input-icon right">
-                            <i class="fa fa-lock"></i>
-                            <input id="password" type="password" class="form-control" name="password">
-                        </div>
-
-                        @if ($errors->has('password'))
-                            <span class="help-block">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
-                        @endif
-                    </div>
-                </div>
-
-                <div class="form-group{{ $errors->has('password_confirmation') ? ' has-error' : '' }}">
-                    <label for="password-confirm" class="col-md-4 control-label">パスワード再入力：</label>
-
-                    <div class="col-md-6">
-                        <input id="password-confirm" type="password" class="form-control" name="password_confirmation">
-
-                        @if ($errors->has('password_confirmation'))
-                            <span class="help-block">
-                                        <strong>{{ $errors->first('password_confirmation') }}</strong>
-                                    </span>
-                        @endif
-                    </div>
-                </div>
-
-
-
-                <div class="form-group mbn">
-                    <div class="col-lg-12" align="right">
-                        <button type="submit" class="btn btn-primary">
-                            <i class="fa fa-btn fa-user"></i> 登録
-                        </button>
-                    </div>
-                </div>
-            </div>
-        </form>
-
         <ol class="breadcrumb page-breadcrumb">
             <li>
                 <i class="fa fa-home"></i>&nbsp;<a href="{{ url('/admin/home') }}">Home</a>&nbsp;&nbsp;<i class="fa fa-angle-right"></i>&nbsp;&nbsp;</li>
@@ -105,6 +26,112 @@
         </div>
     </div>
     <!--END TITLE & BREADCRUMB PAGE-->
+@endsection
+
+@section('content')
+
+    <div class="page-content">
+        <div id="tab-general">
+            <div class="row mbl">
+                <div class="col-lg-12">
+                    <div class="col-md-12">
+                        <div id="area-chart-spline" style="width: 100%; height: 300px; display: none;">
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-lg-12">
+                    <div class="row">
+                        <div class="panel">
+                            <div class="panel-body"><h4></h4>
+
+                                <form action="{{ url('/admin/register') }}" class="form-horizontal" method="POST" role="form">
+                                    {{ csrf_field() }}
+                                    <div class="form-body pal">
+
+                                        <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
+                                            <label for="name" class="col-md-4 control-label">名前</label>
+
+                                            <div class="col-md-6">
+                                                <div class="input-icon right">
+                                                    <i class="fa fa-user"></i>
+                                                    <input id="name" type="text" class="form-control" name="name" value="{{ old('name') }}">
+                                                </div>
+
+                                                @if ($errors->has('name'))
+                                                    <span class="help-block">
+                                        <strong>{{ $errors->first('name') }}</strong>
+                                    </span>
+                                                @endif
+                                            </div>
+                                        </div>
+
+                                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
+                                            <label for="email" class="col-md-4 control-label">
+                                                メールアドレス:</label>
+                                            <div class="col-md-6">
+                                                <input id="email" type="email" placeholder="" class="form-control" name="email" value="{{ old('email') }}"/>
+                                                @if ($errors->has('email'))
+                                                    <span class="help-block">
+                                            <strong>{{ $errors->first('email') }}</strong>
+                                        </span>
+                                                @endif
+                                            </div>
+                                        </div>
+
+
+
+                                        <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
+                                            <label for="password" class="col-md-4 control-label">パスワード：</label>
+
+                                            <div class="col-md-6">
+                                                <div class="input-icon right">
+                                                    <i class="fa fa-lock"></i>
+                                                    <input id="password" type="password" class="form-control" name="password">
+                                                </div>
+
+                                                @if ($errors->has('password'))
+                                                    <span class="help-block">
+                                        <strong>{{ $errors->first('password') }}</strong>
+                                    </span>
+                                                @endif
+                                            </div>
+                                        </div>
+
+                                        <div class="form-group{{ $errors->has('password_confirmation') ? ' has-error' : '' }}">
+                                            <label for="password-confirm" class="col-md-4 control-label">パスワード再入力：</label>
+
+                                            <div class="col-md-6">
+                                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation">
+
+                                                @if ($errors->has('password_confirmation'))
+                                                    <span class="help-block">
+                                        <strong>{{ $errors->first('password_confirmation') }}</strong>
+                                    </span>
+                                                @endif
+                                            </div>
+                                        </div>
+
+
+
+                                        <div class="form-group mbn">
+                                            <div class="col-lg-12" align="right">
+                                                <button type="submit" class="btn btn-primary">
+                                                    <i class="fa fa-btn fa-user"></i> 登録
+                                                </button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </form>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!--END CONTENT-->
 @endsection
 
 
--- resources/views/user/profile.blade.php
+++ resources/views/user/profile.blade.php
@@ -15,76 +15,6 @@
         </div>
         <hr />
 
-        <form action="{{ url('/profile') }}" class="form-horizontal" method="POST" role="form">
-            {{ csrf_field() }}
-            <div class="form-body pal">
-
-                <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
-                    <label class="col-md-4 control-label">名前</label>
-
-                    <div class="col-md-6">
-                        <input type="text" class="form-control" name="name" value="{{ old('name',  Auth::user()->name) }}">
-
-                        @if ($errors->has('name'))
-                            <span class="help-block">
-                                        <strong>{{ $errors->first('name') }}</strong>
-                                    </span>
-                        @endif
-                    </div>
-                </div>
-
-                <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                    <label class="col-md-4 control-label">Eメール</label>
-
-                    <div class="col-md-6">
-                        <input type="email" class="form-control" name="email" value="{{ old('email',  Auth::user()->email ) }}">
-
-                        @if ($errors->has('email'))
-                            <span class="help-block">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                        @endif
-                    </div>
-                </div>
-
-                <div class="form-group{{ $errors->has('class') ? ' has-error' : '' }}">
-                    <label class="col-md-4 control-label">クラス</label>
-
-                    <div class="col-md-6">
-                        {{--<input type="text" class="form-control" name="email" value="{{ old('class',  Auth::user()->class ) }}">--}}
-
-                        <select name="class">
-                            @foreach(\App\class_table::all() as $class )
-
-                                @if($class->id == old('class',  Auth::user()->class ) )
-                                    <option value="{{$class->id}}" selected="selected">{{$class->className}}</option>
-                                @else
-                                    <option value="{{$class->id}}">{{$class->className}}</option>
-                                @endif
-
-
-                            @endforeach
-                        </select>
-
-                        @if ($errors->has('class'))
-                            <span class="help-block">
-                                        <strong>{{ $errors->first('class') }}</strong>
-                                    </span>
-                        @endif
-                    </div>
-                </div>
-
-
-                <div class="form-group">
-                    <div class="col-md-6 col-md-offset-4">
-                        <button type="submit" class="btn btn-primary">
-                            保存
-                        </button>
-                    </div>
-                </div>
-            </div>
-        </form>
-
         <ol class="breadcrumb page-breadcrumb">
             <li>
                 <i class="fa fa-home"></i>&nbsp;<a href="{{ url('/home') }}">Home</a>&nbsp;&nbsp;<i class="fa fa-angle-right"></i>&nbsp;&nbsp;</li>
@@ -94,6 +24,102 @@
         </div>
     </div>
     <!--END TITLE & BREADCRUMB PAGE-->
+@endsection
+
+@section('content')
+
+    <div class="page-content">
+        <div id="tab-general">
+            <div class="row mbl">
+                <div class="col-lg-12">
+                    <div class="col-md-12">
+                        <div id="area-chart-spline" style="width: 100%; height: 300px; display: none;">
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-lg-12">
+                    <div class="row">
+                        <div class="panel">
+                            <div class="panel-body"><h4></h4>
+
+                                <form action="{{ url('/profile') }}" class="form-horizontal" method="POST" role="form">
+                                    {{ csrf_field() }}
+                                    <div class="form-body pal">
+
+                                        <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
+                                            <label class="col-md-4 control-label">名前</label>
+
+                                            <div class="col-md-6">
+                                                <input type="text" class="form-control" name="name" value="{{ old('name',  Auth::user()->name) }}">
+
+                                                @if ($errors->has('name'))
+                                                    <span class="help-block">
+                                        <strong>{{ $errors->first('name') }}</strong>
+                                    </span>
+                                                @endif
+                                            </div>
+                                        </div>
+
+                                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
+                                            <label class="col-md-4 control-label">Eメール</label>
+
+                                            <div class="col-md-6">
+                                                <input type="email" class="form-control" name="email" value="{{ old('email',  Auth::user()->email ) }}">
+
+                                                @if ($errors->has('email'))
+                                                    <span class="help-block">
+                                        <strong>{{ $errors->first('email') }}</strong>
+                                    </span>
+                                                @endif
+                                            </div>
+                                        </div>
+
+                                        <div class="form-group{{ $errors->has('class') ? ' has-error' : '' }}">
+                                            <label class="col-md-4 control-label">クラス</label>
+
+                                            <div class="col-md-6">
+                                                <input type="text" class="form-control" name="email" value="{{ old('class',  Auth::user()->class ) }}">
+
+                                                <select name="class">
+                                                    @foreach(\App\class_table::all() as $class )
+
+                                                        @if($class->id == old('class',  Auth::user()->class ) )
+                                                            <option value="{{$class->id}}" selected="selected">{{$class->className}}</option>
+                                                        @else
+                                                            <option value="{{$class->id}}">{{$class->className}}</option>
+                                                        @endif
+
+
+                                                    @endforeach
+                                                </select>
+
+                                                @if ($errors->has('class'))
+                                                    <span class="help-block">
+                                        <strong>{{ $errors->first('class') }}</strong>
+                                    </span>
+                                                @endif
+                                            </div>
+                                        </div>
+
+
+                                        <div class="form-group">
+                                            <div class="col-md-6 col-md-offset-4">
+                                                <button type="submit" class="btn btn-primary">
+                                                    保存
+                                                </button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!--END CONTENT-->
 @endsection
 
 
