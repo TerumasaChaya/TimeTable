ae267eebfeb0210a1eaaea8baf0daf14535f6d1b
茶谷 晃昌 <2130260@maetel.ecc.ac.jp>
2016-07-26 16:51:36 +0900

    サイドバー修正

--- resources/views/admin/elements/side-bar.blade.php
+++ resources/views/admin/elements/side-bar.blade.php
@@ -4,26 +4,6 @@
          data-position="right" class="navbar-default navbar-static-side">
         <div class="sidebar-collapse menu-scroll">
             <ul id="side-menu" class="nav">
-
-                <div class="clearfix"></div>
-                <li>
-                    <a href="dashboard.html">
-                        <i class="fa fa-tachometer fa-fw">
-                            <div class="icon-bg bg-orange"></div>
-                        </i>
-                        <span class="menu-title">ここにページのタイトルが入る</span>
-                    </a>
-                </li>
-
-                <li>
-                    <a href="Layout.html">
-                        <i class="fa fa-desktop fa-fw">
-                            <div class="icon-bg bg-pink"></div>
-                        </i>
-                        <span class="menu-title">ここにページのタイトルが入る</span>
-                    </a>
-                </li>
-
                 <li>
                     <a href="/admin/teacher">
                         <i class="fa fa-users fa-fw">
@@ -32,69 +12,6 @@
                         <span class="menu-title">教師一覧</span>
                     </a>
                 </li>
-
-                <li>
-                    <a href="Forms.html">
-                        <i class="fa fa-edit fa-fw">
-                            <div class="icon-bg bg-violet"></div>
-                        </i>
-                        <span class="menu-title">ここにページのタイトルが入る</span>
-                    </a>
-                </li>
-
-                <li>
-                    <a href="Tables.html">
-                        <i class="fa fa-th-list fa-fw">
-                            <div class="icon-bg bg-blue"></div>
-                        </i>
-                        <span class="menu-title">ここにページのタイトルが入る</span>
-                    </a>
-                </li>
-
-                <li>
-                    <a href="DataGrid.html">
-                        <i class="fa fa-database fa-fw">
-                            <div class="icon-bg bg-red"></div>
-                        </i>
-                        <span class="menu-title">ここにページのタイトルが入る</span>
-                    </a>
-                </li>
-
-                <li>
-                    <a href="Pages.html">
-                        <i class="fa fa-file-o fa-fw">
-                            <div class="icon-bg bg-yellow"></div>
-                        </i>
-                        <span class="menu-title">ここにページのタイトルが入る</span>
-                    </a>
-                </li>
-
-                <li>
-                    <a href="Extras.html">
-                        <i class="fa fa-gift fa-fw">
-                            <div class="icon-bg bg-grey"></div>
-                        </i>
-                        <span class="menu-title">ここにページのタイトルが入る</span>
-                    </a>
-                </li>
-
-                <li><a href="Dropdown.html">
-                        <i class="fa fa-sitemap fa-fw">
-                            <div class="icon-bg bg-dark"></div>
-                        </i>
-                        <span class="menu-title">ここにページのタイトルが入る</span>
-                    </a>
-                </li>
-
-                <li>
-                    <a href="Email.html">
-                        <i class="fa fa-envelope-o">
-                            <div class="icon-bg bg-primary"></div>
-                        </i>
-                        <span class="menu-title">ここにページのタイトルが入る</span>
-                    </a>
-                </li>
-
                 <li>
                     <a href="/admin/delete">
                         <i class="fa fa-trash-o fa-fw">
--- resources/views/user/elements/side-bar.blade.php
+++ resources/views/user/elements/side-bar.blade.php
@@ -41,69 +41,6 @@
                         <span class="menu-title">教室一覧</span>
                     </a>
                 </li>
-
-                <li>
-                    <a href="Tables.html">
-                        <i class="fa fa-th-list fa-fw">
-                            <div class="icon-bg bg-blue"></div>
-                        </i>
-                        <span class="menu-title">ここにページのタイトルが入る</span>
-                    </a>
-                </li>
-
-                <li>
-                    <a href="DataGrid.html">
-                        <i class="fa fa-database fa-fw">
-                            <div class="icon-bg bg-red"></div>
-                        </i>
-                        <span class="menu-title">ここにページのタイトルが入る</span>
-                    </a>
-                </li>
-
-                <li>
-                    <a href="Pages.html">
-                        <i class="fa fa-file-o fa-fw">
-                            <div class="icon-bg bg-yellow"></div>
-                        </i>
-                        <span class="menu-title">ここにページのタイトルが入る</span>
-                    </a>
-                </li>
-
-                <li>
-                    <a href="Extras.html">
-                        <i class="fa fa-gift fa-fw">
-                            <div class="icon-bg bg-grey"></div>
-                        </i>
-                        <span class="menu-title">ここにページのタイトルが入る</span>
-                    </a>
-                </li>
-
-                <li><a href="Dropdown.html">
-                        <i class="fa fa-sitemap fa-fw">
-                            <div class="icon-bg bg-dark"></div>
-                        </i>
-                        <span class="menu-title">ここにページのタイトルが入る</span>
-                    </a>
-                </li>
-
-                <li>
-                    <a href="Email.html">
-                        <i class="fa fa-envelope-o">
-                            <div class="icon-bg bg-primary"></div>
-                        </i>
-                        <span class="menu-title">ここにページのタイトルが入る</span>
-                    </a>
-                </li>
-
-                <li>
-                    <a href="Charts.html">
-                        <i class="fa fa-bar-chart-o fa-fw">
-                            <div class="icon-bg bg-orange"></div>
-                        </i>
-                        <span class="menu-title">ここにページのタイトルが入る</span>
-                    </a>
-                </li>
-
                 <li>
                     <a href="/user/attendance">
                         <i class="fa fa-list-alt fa-fw">
